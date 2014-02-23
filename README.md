792090ae63985f43e8f21f1fe929daed0df7c883
jesse keane <me@jessekeane.me>
2014-02-22 17:20:44 -0800

    add fuzziness and readme and test!

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 remorse
 =====
+
+turn timing information into morse code
+
+## usage
+
+remorse is a through stream that takes arrays of timing information and turns
+them into morse code.
+
+remorse accepts two optional parameters:
+
+* the first is what is to be considered the time unit (defaults to 500ms)
+* the second is the "fuzziness allowance", as in: "within how many milliseconds
+can we smudge the timing to fit our idea of morse code" (defaults to 200ms)
+
+## example
+
+```js
+var remorse = require('remorse'),
+    fs = require('fs')
+
+fs.createReadStream('mouse-timing-events.dat')
+  .pipe(remorse(225, 100))
+  .pipe(fs.createWriteStream('secret-morse-codezzz.txt'))
+```
+
+## license
+
+MIT
--- index.js
+++ index.js
@@ -2,14 +2,17 @@ var through = require('through')
 
 module.exports = remorse
 
-function remorse(time_unit) {
-  var stream = through(process)
+function remorse(_time_unit, _fuzziness) {
+  var time_unit = _time_unit || 500,
+      fuzziness = _fuzziness || 200
 
   var DOT = time_unit,
       DASH = time_unit * 3,
       INTER_DOTDASH = time_unit,
       INTER_LETTER = time_unit * 3,
       INTER_WORD = time_unit * 7
+
+  var stream = through(process)
 
   var current_word = [],
       conversion = {}
@@ -20,8 +23,8 @@ function remorse(time_unit) {
   return stream
 
   function process(data) {
-    var character = data[0],
-        space = data[1]
+    var character = defuzz(data[0]),
+        space = defuzz(data[1])
 
     current_word.push(conversion[character])
     if (space === INTER_WORD) {
@@ -29,5 +32,12 @@ function remorse(time_unit) {
       return current_word = []
     }
     if (space === INTER_LETTER) current_word.push(' ')
+  }
+
+  function defuzz(time) {
+    if (time < (DOT + fuzziness)) return DOT
+    if (Math.abs(time - DASH) <= fuzziness) return DASH
+
+    return INTER_WORD
   }
 }
